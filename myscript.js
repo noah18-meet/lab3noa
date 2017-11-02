dfa844ef4365c6f1b175821cf33f47e4b794e38a
noah18-meet <noa.niki4@gmail.com>
2017-11-02 18:44:24 +0200

    saving lab 3

---
+++ myscript.js
@@ -0,0 +1,8 @@
+alert("noa");
+var a=prompt("whold you like a blue background or a pink one?");
+if(a=="blue"){
+ document.getElementsByTagName('body').style.background-color="blue";
+}
+else if(a=="pink"){
+ document.getElementsByTagName('body').style.background-color="pink";
+}
